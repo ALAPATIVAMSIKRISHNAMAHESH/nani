cc3ce9c8e3aed466916e0be31238ae7094f48a01
ALAPATIVAMSIKRISHNAMAHESH <alapativamsi28@gmail.com>
2020-03-12 15:40:19 +0530

    Updates

---
+++ precache-manifest.3d13f0235eeaac6dee6395bc26b14b47.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d9e772edbead2179f2621f0bcb4a09d",
+    "url": "/nani/index.html"
+  },
+  {
+    "revision": "2400cd297bb705caefc9",
+    "url": "/nani/static/css/main.3bbc7bc1.chunk.css"
+  },
+  {
+    "revision": "f407382bea19b5c9dd55",
+    "url": "/nani/static/js/2.41cde6ae.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/nani/static/js/2.41cde6ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2400cd297bb705caefc9",
+    "url": "/nani/static/js/main.34959f4b.chunk.js"
+  },
+  {
+    "revision": "fff2663fb820eaf1e4c5",
+    "url": "/nani/static/js/runtime-main.153c9eab.js"
+  },
+  {
+    "revision": "1828474731af76242ae063b2c09a6943",
+    "url": "/nani/static/media/businessman.18284747.svg"
+  }
+]);
